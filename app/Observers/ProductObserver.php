57e87cbe91b14e3d4505a2cd4c042e81ab7f7a0d
Alexander Andreiev <alexander.a@code-care.pro>
2023-01-31 23:29:35 +0100

    products update observer adding & some errors fixing

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
+
+class ProductObserver
+{
+    public function updated(Product $product) {
+        // Check if the image field has changed
+        if ($product->isDirty('image')) {
+            // Delete the old image from storage
+            Storage::delete($product->getOriginal('image'));
+
+            // Put the new image in storage
+            $product->image->store('images');
+        }
+    }
+}
